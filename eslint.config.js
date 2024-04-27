771781f47cb199f9e9cc9a6c977eafc8e176aa5a
Clukva <berries.whale@gmail.com>
2024-04-27 16:52:47 +0300

    feat: configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+const config = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  }
+)
+
+export default config
