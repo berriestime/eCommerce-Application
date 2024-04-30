fa9d11532fe9d29d5035ce7c59ede96d3714c150
Clukva <berries.whale@gmail.com>
2024-04-30 14:18:54 +0300

    feat: add links to pages

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,9 +1,14 @@
 import type { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 import s from './login-page.module.css';
 
 const LoginPage: FC = () => {
-  return <div className={s.container}>Login Page</div>;
+  return (
+    <div className={s.container}>
+      <Link to="/">Go to Main Page</Link> Login Page
+    </div>
+  );
 };
 
 export { LoginPage };
--- src/pages/root-page/root-page.tsx
+++ src/pages/root-page/root-page.tsx
@@ -1,9 +1,15 @@
 import type { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 import s from './root-page.module.css';
 
 const RootPage: FC = () => {
-  return <div className={s.container}>Root Page</div>;
+  return (
+    <div className={s.container}>
+      {' '}
+      <Link to="/login">Go to Login Page</Link>Root Page
+    </div>
+  );
 };
 
 export { RootPage };
