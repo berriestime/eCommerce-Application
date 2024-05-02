2a97c9f7af9bf4e87992cdbcd45086f7da9b727c
Clukva <berries.whale@gmail.com>
2024-05-02 13:23:15 +0300

    refactor: improve rules for commit messages

---
+++ commitlint.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+    'header-max-length': [2, 'always', 120],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [2, 'always', ['build', 'ci', 'docs', 'feat', 'fix', 'refactor', 'revert', 'style', 'test', 'init']],
+  },
+};
