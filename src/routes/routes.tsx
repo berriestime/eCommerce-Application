da3c243323e9121f254010651443ace1cd566e54
Clukva <berries.whale@gmail.com>
2024-05-04 15:18:23 +0300

    refactor: remove extra unneeded router

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,22 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
+import { RouteObject } from 'react-router-dom';
 
 import { authorizedRoutes } from './authorized';
 import { commonRoutes } from './common';
 import { unauthorizedRoutes } from './unauthorized';
 
-export const routes = [
-  {
-    element: (
-      <Routes>
-        {[...unauthorizedRoutes, ...(isAuthenticated() ? authorizedRoutes : []), ...commonRoutes].map((route) => (
-          <Route element={route.element} key={route.path} path={route.path} />
-        ))}
-      </Routes>
-    ),
-    path: '*',
-  },
-];
+const makeRoutes = (): RouteObject[] => {
+  const res = [...commonRoutes];
+  if (isAuthenticated()) {
+    res.push(...authorizedRoutes);
+  } else {
+    res.push(...unauthorizedRoutes);
+  }
+  return res;
+};
+
+const routes = makeRoutes();
 
 function isAuthenticated(): boolean {
   return false;
 }
+
+export { routes };
