8d53289b88b26caf51d480b45f4466172b846097
Naya252 <naya252naya@gmail.com>
2024-05-05 22:49:51 +0300

    feat: implement mantine

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,7 +1,16 @@
 import { RouterProvider } from 'react-router-dom';
 
+import { MantineProvider } from '@mantine/core';
+
 import { router } from '@/routes';
+import { theme } from '@/theme';
+
+import '@mantine/core/styles.css';
 
 export const AppProvider = (): JSX.Element => {
-  return <RouterProvider router={router} />;
+  return (
+    <MantineProvider defaultColorScheme="dark" theme={theme}>
+      <RouterProvider router={router} />
+    </MantineProvider>
+  );
 };
