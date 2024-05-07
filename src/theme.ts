56b4be090379e95ce5650f35fefc32b61954044d
Naya252 <naya252naya@gmail.com>
2024-05-08 01:29:01 +0300

    fix: change font of the headings

--- src/theme.ts
+++ src/theme.ts
@@ -4,7 +4,7 @@ export const theme = createTheme({
   autoContrast: true,
   black: '#1f1b1b',
   fontFamily: 'Inter, sans-serif',
-  headings: { fontFamily: 'Martel, sans-serif' },
+  headings: { fontFamily: 'Martel' },
   primaryColor: 'red',
   white: '#f3e7e4',
 });
