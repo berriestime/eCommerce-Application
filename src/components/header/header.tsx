8f1e7717d1acaa7b7a42bb0cab51bf13ebbee9f9
Naya252 <naya252naya@gmail.com>
2024-05-07 10:27:22 +0300

    refactor: add clsx & use dry

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 import { Box, Burger, Divider, Drawer, Group, ScrollArea, rem } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { clsx } from 'clsx';
 
 import { Logo } from '@/components/logo';
 
@@ -10,6 +11,38 @@ import classes from './header.module.css';
 
 const Header: FC = () => {
   const [drawerOpened, { close: closeDrawer, toggle: toggleDrawer }] = useDisclosure(false);
+  const nav = [
+    { name: 'Main', to: '/' },
+    { name: 'Store', to: '/catalog' },
+    { name: 'Cart', to: '/cart' },
+    { name: 'Our Team', to: '/team' },
+  ];
+
+  const auth = [
+    { name: 'Log In', to: '/login' },
+    { name: 'Sign Up', to: '/registration' },
+  ];
+
+  const getItems = (
+    elements: {
+      name: string;
+      to: string;
+    }[],
+    curClass: string | undefined,
+  ): JSX.Element[] => {
+    const items = elements.map((el, index) => (
+      <NavLink
+        className={({ isActive }) => clsx(curClass, { [classes.active || '']: isActive })}
+        key={index}
+        onClick={closeDrawer}
+        to={el.to}
+      >
+        {el.name}
+      </NavLink>
+    ));
+
+    return items;
+  };
 
   return (
     <Box className={classes.container}>
@@ -17,37 +50,10 @@ const Header: FC = () => {
         <Logo />
 
         <Group gap={0} h="100px" visibleFrom="sm">
-          <NavLink className={({ isActive }) => (isActive ? `${classes.link} ${classes.active}` : classes.link)} to="/">
-            Main
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? `${classes.link} ${classes.active}` : classes.link)}
-            to="/catalog"
-          >
-            Store
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? `${classes.link} ${classes.active}` : classes.link)}
-            to="/cart"
-          >
-            Cart
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? `${classes.link} ${classes.active}` : classes.link)}
-            to="/team"
-          >
-            Our Team
-          </NavLink>
+          {getItems(nav, classes.link)}
         </Group>
 
-        <Group visibleFrom="sm">
-          <NavLink className={classes.authLink} to="/login">
-            Log In
-          </NavLink>
-          <NavLink className={classes.authLink} to="/registration">
-            Sign Up
-          </NavLink>
-        </Group>
+        <Group visibleFrom="sm">{getItems(auth, classes.authLink)}</Group>
 
         <Burger hiddenFrom="sm" onClick={toggleDrawer} opened={drawerOpened} />
       </Group>
@@ -64,37 +70,12 @@ const Header: FC = () => {
         <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
           <Divider my="sm" />
 
-          <NavLink className={({ isActive }) => (isActive ? `${classes.link} ${classes.active}` : classes.link)} to="/">
-            Main
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? `${classes.link} ${classes.active}` : classes.link)}
-            to="/catalog"
-          >
-            Store
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? `${classes.link} ${classes.active}` : classes.link)}
-            to="/cart"
-          >
-            Cart
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? `${classes.link} ${classes.active}` : classes.link)}
-            to="/team"
-          >
-            Our Team
-          </NavLink>
+          {getItems(nav, classes.link)}
 
           <Divider my="sm" />
 
           <Group grow justify="center" pb="xl" px="md">
-            <NavLink className={classes.link} to="/login">
-              Log In
-            </NavLink>
-            <NavLink className={classes.link} to="/registration">
-              Sign Up
-            </NavLink>
+            {getItems(auth, classes.authLink)}
           </Group>
         </ScrollArea>
       </Drawer>
