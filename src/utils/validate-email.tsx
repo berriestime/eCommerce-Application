9e6cb25fb9b6626838ee9dc282d04e01aaeaee01
NadyaGus <ftotkexit@gmail.com>
2024-05-08 21:24:54 +0300

    refactor: add variables for magic strings

--- src/utils/validate-email.tsx
+++ src/utils/validate-email.tsx
@@ -1,22 +1,28 @@
+const MESSAGE_EMAIL_REQUIRED = 'Email is required';
+const MESSAGE_EMAIL_NO_TRAILING_WHITESPACE = 'Email address must not contain leading or trailing whitespace';
+const MESSAGE_EMAIL_CONTAIN_AT_SYMBOL = "Email address must contain an '@' symbol";
+const MESSAGE_EMAIL_MUST_FORMATTED = 'Email address must be properly formatted (e.g., user@example.com)';
+const MESSAGE_EMAIL_NEED_DOMAIN = 'Email address must contain a domain name (e.g., example.com)';
+
 const validateEmail = (value: string): null | string => {
   if (value.length === 0) {
-    return 'Email is required';
+    return MESSAGE_EMAIL_REQUIRED;
   }
 
   if (value[0] === ' ' || value.at(-1) === ' ') {
-    return 'Email address must not contain leading or trailing whitespace';
+    return MESSAGE_EMAIL_NO_TRAILING_WHITESPACE;
   }
 
   if (!/(\w*[@]\w*)/.test(value)) {
-    return "Email address must contain an '@' symbol";
+    return MESSAGE_EMAIL_CONTAIN_AT_SYMBOL;
   }
 
   if (!/^\S+@\S+$/.test(value)) {
-    return 'Email address must be properly formatted (e.g., user@example.com)';
+    return MESSAGE_EMAIL_MUST_FORMATTED;
   }
 
   if (!/(\w*@+[a-zA-Z-0-9].[a-zA-Z]\w*)/.test(value)) {
-    return 'Email address must contain a domain name (e.g., example.com)';
+    return MESSAGE_EMAIL_NEED_DOMAIN;
   }
 
   return null;
--- src/utils/validate-password.tsx
+++ src/utils/validate-password.tsx
@@ -1,32 +1,40 @@
 const MIN_PASSWORD_LENGTH = 8;
 
+const MESSAGE_PASSWORD_REQUIRED = 'Password is required';
+const MESSAGE_PASSWORD_NO_TRAILING_WHITESPACE = 'Password must not contain leading or trailing whitespace';
+const MESSAGE_PASSWORD_LENGTH_ERROR = 'Password must be at least 8 characters long';
+const MESSAGE_PASSWORD_CONTAIN_UPPERCASE = 'Password must contain at least one uppercase letter';
+const MESSAGE_PASSWORD_CONTAIN_LOWERCASE = 'Password must contain at least one lowercase letter';
+const MESSAGE_PASSWORD_CONTAIN_DIGIT = 'Password must contain at least one digit';
+const MESSAGE_PASSWORD_CONTAIN_SPECIAL_CHARACTER = 'Password must contain at least one special character (@#$%^&*)';
+
 const validatePassword = (value: string): null | string => {
   if (value.length === 0) {
-    return 'Password is required';
+    return MESSAGE_PASSWORD_REQUIRED;
   }
 
   if (value[0] === ' ' || value.at(-1) === ' ') {
-    return 'Password must not contain leading or trailing whitespace';
+    return MESSAGE_PASSWORD_NO_TRAILING_WHITESPACE;
   }
 
   if (!(value.length >= MIN_PASSWORD_LENGTH)) {
-    return 'Password must be at least 8 characters long';
+    return MESSAGE_PASSWORD_LENGTH_ERROR;
   }
 
   if (!/(\w*[A-Z]\w*)/.test(value)) {
-    return 'Password must contain at least one uppercase letter';
+    return MESSAGE_PASSWORD_CONTAIN_UPPERCASE;
   }
 
   if (!/(\w*[a-z]\w*)/.test(value)) {
-    return 'Password must contain at least one lowercase letter';
+    return MESSAGE_PASSWORD_CONTAIN_LOWERCASE;
   }
 
   if (!/(\w*[0-9]\w*)/.test(value)) {
-    return 'Password must contain at least one digit';
+    return MESSAGE_PASSWORD_CONTAIN_DIGIT;
   }
 
   if (!/(\w*[!@#$%^&*]\w*)/.test(value)) {
-    return 'Password must contain at least one special character (@#$%^&*)';
+    return MESSAGE_PASSWORD_CONTAIN_SPECIAL_CHARACTER;
   }
 
   return null;
