4edc44c8a24a09dc43c9f68c29481f77b4643a06
Naya252 <naya252naya@gmail.com>
2024-05-10 00:42:59 +0300

    feat: implement carousel to main page

--- src/features/root-page/components/cards-section/cards-section.tsx
+++ src/features/root-page/components/cards-section/cards-section.tsx
@@ -1,7 +1,10 @@
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Box, SimpleGrid } from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
+import { Box, SimpleGrid, rem } from '@mantine/core';
 import { clsx } from 'clsx';
+import Autoplay from 'embla-carousel-autoplay';
 
 import { type CardData, SimpleCard } from '@/components/simple-card';
 
@@ -16,20 +19,45 @@ type Info = {
 
 const CardsSection = (props: Info): JSX.Element => {
   const { mockdata, text, title } = props;
+  const autoplay = useRef(Autoplay({ delay: 5000 }));
 
-  const producCards = mockdata.map((producCard) => (
-    <Link className={cardClasses.link} key={producCard.title} to={producCard.to}>
-      <SimpleCard {...producCard} />
+  const productCards = mockdata.map((productCard) => (
+    <Link className={cardClasses.link} key={productCard.title} to={productCard.to}>
+      <SimpleCard {...productCard} />
     </Link>
   ));
+
+  const slides = productCards.map((item, i) => <Carousel.Slide key={i}>{item}</Carousel.Slide>);
 
   return (
     <Box className={classes.container}>
       <h3 className={clsx(classes.title, classes.infoContent, { [classes.textPadding || '']: !text })}>{title}</h3>
       <p className={clsx(classes.text, classes.infoContent, { [classes.textPadding || '']: text })}>{text}</p>
-      <SimpleGrid className={cardClasses.cardsGap} cols={3} mt="md">
-        {producCards}
+
+      <SimpleGrid className={cardClasses.cardsGap} cols={3} mt="md" visibleFrom="md">
+        {productCards}
       </SimpleGrid>
+
+      <Carousel
+        align="start"
+        classNames={{
+          controls: cardClasses.carouselControls,
+          indicator: cardClasses.carouselIndicator,
+          indicators: cardClasses.carouselIndicators,
+          root: cardClasses.carousel,
+        }}
+        hiddenFrom="md"
+        loop
+        onMouseEnter={autoplay.current.stop}
+        onMouseLeave={autoplay.current.reset}
+        plugins={[autoplay.current]}
+        slideGap={{ base: rem(2) }}
+        slideSize={{ base: '100%' }}
+        slidesToScroll={1}
+        withIndicators
+      >
+        {slides}
+      </Carousel>
     </Box>
   );
 };
--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -6,6 +6,7 @@ import { router } from '@/routes';
 import { theme } from '@/theme';
 
 import '@mantine/core/styles.css';
+import '@mantine/carousel/styles.css';
 
 export const AppProvider = (): JSX.Element => {
   return (
