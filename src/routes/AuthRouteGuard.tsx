a6deea1b19463acbb83fb03ba71b6a4cf1334348
Naya252 <naya252naya@gmail.com>
2024-05-15 23:47:59 +0300

    refactor: change route guard

--- src/routes/AuthRouteGuard.tsx
+++ src/routes/AuthRouteGuard.tsx
@@ -1,7 +1,13 @@
 import type { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 
+import { Box, Loader } from '@mantine/core';
+
+import { useAuth } from '@/hooks';
+
 import type { AuthState } from '../types/authState';
+
+import classes from '@/components/loader/loader.module.css';
 
 type Props = {
   authRejectStatus: AuthState;
@@ -10,7 +16,15 @@ type Props = {
 };
 
 export const AuthRouteGuard = ({ authRejectStatus, children, rejectRoute }: Props): ReactNode => {
-  const authState = 'UNAUTHENTICATED';
+  const authState = useAuth();
+
+  if (authState === 'PENDING') {
+    return (
+      <Box className={classes.box}>
+        <Loader />
+      </Box>
+    );
+  }
 
   if (authState === authRejectStatus) {
     return <Navigate replace={true} to={rejectRoute} />;
