a2492bf10fbeb100a20ce6076f1331766d030c45
NadyaGus <ftotkexit@gmail.com>
2024-05-18 19:39:22 +0300

    feat: change validate error message

--- src/utils/validate-password.tsx
+++ src/utils/validate-password.tsx
@@ -1,12 +1,12 @@
 const MIN_PASSWORD_LENGTH = 8;
 
 const MESSAGE_PASSWORD_REQUIRED = 'Password is required';
-const MESSAGE_PASSWORD_NO_TRAILING_WHITESPACE = 'Password must not contain leading or trailing whitespace';
-const MESSAGE_PASSWORD_LENGTH_ERROR = 'Password must be at least 8 characters long';
-const MESSAGE_PASSWORD_CONTAIN_UPPERCASE = 'Password must contain at least one uppercase letter';
-const MESSAGE_PASSWORD_CONTAIN_LOWERCASE = 'Password must contain at least one lowercase letter';
-const MESSAGE_PASSWORD_CONTAIN_DIGIT = 'Password must contain at least one digit';
-const MESSAGE_PASSWORD_CONTAIN_SPECIAL_CHARACTER = 'Password must contain at least one special character (@#$%^&*)';
+const MESSAGE_PASSWORD_NO_TRAILING_WHITESPACE = 'Must not contain leading or trailing whitespace';
+const MESSAGE_PASSWORD_LENGTH_ERROR = 'Must be at least 8 characters long';
+const MESSAGE_PASSWORD_CONTAIN_UPPERCASE = 'Must contain at least one uppercase letter';
+const MESSAGE_PASSWORD_CONTAIN_LOWERCASE = 'Must contain at least one lowercase letter';
+const MESSAGE_PASSWORD_CONTAIN_DIGIT = 'Must contain at least one digit';
+const MESSAGE_PASSWORD_CONTAIN_SPECIAL_CHARACTER = 'Must contain at least one special character (@#$%^&*)';
 
 const validatePassword = (value: string): null | string => {
   if (value.length === 0) {
