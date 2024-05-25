e27ccb0b91641dcea1c1a23806cdbf5d3a500aed
Naya252 <naya252naya@gmail.com>
2024-05-25 19:39:04 +0300

    refactor: change stucture

--- src/features/catalog/product/product-page.tsx
+++ src/features/catalog/product/product-page.tsx
@@ -3,7 +3,8 @@ import { useLoaderData } from 'react-router-dom';
 
 import { Product } from '@commercetools/platform-sdk';
 import { Carousel } from '@mantine/carousel';
-import { Box, Grid, Image, SimpleGrid, Spoiler, Text, Title } from '@mantine/core';
+import { Box, Flex, Grid, Image, SimpleGrid, Spoiler, Text, Title } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { clsx } from 'clsx';
 
 import { Breadcrumbs } from '@/components/brearcrumbs';
@@ -15,12 +16,27 @@ import classes from './product-page.module.css';
 
 const ProductPage: FC = () => {
   const data = useLoaderData();
+  const matchesXs = useMediaQuery('(width < 48em)');
+  const matchesXxs = useMediaQuery('(width < 22.5em)');
 
   const { productData } = data as { productData: Product };
   console.log('PRODUCT', data);
 
   const { discountPrice, price } = getPrice(productData);
   const { images } = productData.masterData.current.masterVariant;
+  console.log(images);
+
+  const cards = [1, 2, 3, 4, 5, 6].map((el) => (
+    <Box bg="white" h="482" key={el}>
+      {el}
+    </Box>
+  ));
+
+  const title = (
+    <Title c="bright" className={classes.productTitle}>
+      {productData.masterData.current.name['en-US']}
+    </Title>
+  );
 
   return (
     <>
@@ -31,82 +47,49 @@ const ProductPage: FC = () => {
             inner: classes.gridInner,
             root: classes.grid,
           }}
-          gutter={0}
+          gutter={matchesXs ? 20 : 60}
           pb={100}
         >
-          <Grid.Col
-            className={classes.col}
-            hiddenFrom="sm"
-            pos="relative"
-            span={{ base: 12, md: 'auto', sm: 'auto', xs: 12 }}
-          >
-            <Title c="bright" className={classes.productTitle} order={1}>
-              {productData.masterData.current.name['en-US']}
-            </Title>
-          </Grid.Col>
-          <Grid.Col
-            className={clsx(classes.previewCol, classes.col)}
-            pos="relative"
-            span={{ base: 12, md: 'content', sm: 12, xs: 12 }}
-            visibleFrom="md"
-          >
-            <Box h="100%" w={60}>
-              <Carousel
-                align="start"
-                classNames={{
-                  controls: classes.carouselControls,
-                  indicator: classes.carouselIndicator,
-                  indicators: classes.carouselIndicators,
-                  root: classes.carousel,
-                }}
-                height={200}
-                loop
-                orientation="vertical"
-                slideGap="xl"
-                slideSize="33.3%"
-                slidesToScroll={3}
-              >
-                <Carousel.Slide bg="blue">1</Carousel.Slide>
-                <Carousel.Slide bg="cyan">2</Carousel.Slide>
-                <Carousel.Slide bg="grape">3</Carousel.Slide>
-                <Carousel.Slide bg="indigo">4</Carousel.Slide>
-                <Carousel.Slide bg="lime">5</Carousel.Slide>
-                <Carousel.Slide bg="pink">6</Carousel.Slide>
-              </Carousel>
-            </Box>
-          </Grid.Col>
-          <Grid.Col className={classes.col} pos="relative" span={{ base: 12, md: 'auto', sm: 'auto', xs: 12 }}>
-            <Image alt={'alt'} className={classes.image} fit="contain" src={images?.[0]?.url} />
+          <Grid.Col className={classes.col} pos="relative" span={{ base: 12, sm: 7, xs: 6 }}>
+            <Flex
+              direction={matchesXxs ? 'column' : 'row'}
+              gap={{ base: 'sm', md: 60, sm: 20 }}
+              justify={{ sm: 'flex-start' }}
+            >
+              <Box h="100%">
+                <Carousel
+                  align="start"
+                  classNames={{
+                    controls: classes.carouselControls,
+                    indicator: classes.carouselIndicator,
+                    indicators: classes.carouselIndicators,
+                    root: classes.carousel,
+                  }}
+                  height={matchesXxs ? 60 : '100%'}
+                  loop
+                  orientation={matchesXxs ? 'horizontal' : 'vertical'}
+                  slideGap="xl"
+                  slideSize={60}
+                  slidesToScroll={1}
+                  w={matchesXxs ? '100%' : 60}
+                >
+                  <Carousel.Slide bg="blue">1</Carousel.Slide>
+                  <Carousel.Slide bg="cyan">2</Carousel.Slide>
+                  <Carousel.Slide bg="grape">3</Carousel.Slide>
+                  <Carousel.Slide bg="indigo">4</Carousel.Slide>
+                  <Carousel.Slide bg="lime">5</Carousel.Slide>
+                  <Carousel.Slide bg="pink">6</Carousel.Slide>
+                </Carousel>
+              </Box>
 
-            <Box hiddenFrom="md">
-              <Carousel
-                align="start"
-                classNames={{
-                  controls: classes.carouselControls,
-                  indicator: classes.carouselIndicator,
-                  indicators: classes.carouselIndicators,
-                  root: classes.carousel,
-                }}
-                height={60}
-                loop
-                orientation="horizontal"
-                slideGap="xl"
-                slideSize="33.3%"
-                slidesToScroll={3}
-              >
-                <Carousel.Slide bg="blue">1</Carousel.Slide>
-                <Carousel.Slide bg="cyan">2</Carousel.Slide>
-                <Carousel.Slide bg="grape">3</Carousel.Slide>
-                <Carousel.Slide bg="indigo">4</Carousel.Slide>
-                <Carousel.Slide bg="lime">5</Carousel.Slide>
-                <Carousel.Slide bg="pink">6</Carousel.Slide>
-              </Carousel>
-            </Box>
+              <Box h="100%">
+                <Image alt={'alt'} className={classes.image} fit="contain" src={images?.[0]?.url} />
+              </Box>
+            </Flex>
           </Grid.Col>
-          <Grid.Col className={classes.col} pos="relative" span={{ base: 12, md: 'auto', sm: 'auto', xs: 12 }}>
-            <Title c="bright" className={classes.productTitle} visibleFrom="sm">
-              {productData.masterData.current.name['en-US']}
-            </Title>
+
+          <Grid.Col className={classes.col} pos="relative" span={{ base: 12, sm: 5, xs: 6 }}>
+            {title}
 
             <Title mb={20} mt={16}>
               {discountPrice && <span className={clsx(classes.price, classes.discount)}>{discountPrice} $</span>}
@@ -133,28 +116,7 @@ const ProductPage: FC = () => {
         </Text>
 
         <SimpleGrid className={classes.cardsGap} cols={3} mt="xl" spacing="60">
-          <Box bg="white" h="482">
-            1
-          </Box>
-
-          <Box bg="white" h="482">
-            1
-          </Box>
-
-          <Box bg="white" h="482">
-            1
-          </Box>
-          <Box bg="white" h="482">
-            1
-          </Box>
-
-          <Box bg="white" h="482">
-            1
-          </Box>
-
-          <Box bg="white" h="482">
-            1
-          </Box>
+          {cards}
         </SimpleGrid>
       </Box>
 
