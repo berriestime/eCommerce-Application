4ffa939d9eed2ba6da3e1af74f0920311be724a8
Naya252 <naya252naya@gmail.com>
2024-05-27 21:23:45 +0300

    fix: change catalog page bc

--- src/features/catalog/catalog-data-loader.ts
+++ src/features/catalog/catalog-data-loader.ts
@@ -1,23 +1,14 @@
-import {
-  CategoryPagedQueryResponse,
-  ClientResponse,
-  ProductProjectionPagedQueryResponse,
-} from '@commercetools/platform-sdk';
+import { ClientResponse, ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk';
 
-import { getAllCategories } from './api/category-api';
 import { getAllProducts } from './api/product-api';
 
 async function loader(): Promise<{
-  categoriesData: CategoryPagedQueryResponse;
   productsData: ProductProjectionPagedQueryResponse;
 }> {
   const productsResponse: ClientResponse<ProductProjectionPagedQueryResponse> = await getAllProducts();
   const productsData: ProductProjectionPagedQueryResponse = productsResponse.body;
 
-  const categoriesResponse: ClientResponse<CategoryPagedQueryResponse> = await getAllCategories();
-  const categoriesData: CategoryPagedQueryResponse = categoriesResponse.body;
-
-  return { categoriesData, productsData };
+  return { productsData };
 }
 
 export { loader };
--- src/features/catalog/catalog-page.tsx
+++ src/features/catalog/catalog-page.tsx
@@ -1,29 +1,23 @@
 import type { FC } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 
-import { CategoryPagedQueryResponse, ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk';
+import { ProductProjectionPagedQueryResponse } from '@commercetools/platform-sdk';
 import { Box, SimpleGrid, Title } from '@mantine/core';
 
 import { Breadcrumbs } from '@/components/brearcrumbs';
 import { Footer } from '@/components/footer';
 import { CommonCard } from '@/components/product-card/common-card';
 
+import { Tabs } from './category/components';
+
 const CatalogPage: FC = () => {
   const data = useLoaderData();
 
-  const { categoriesData, productsData } = data as {
-    categoriesData: CategoryPagedQueryResponse;
+  const { productsData } = data as {
     productsData: ProductProjectionPagedQueryResponse;
   };
 
-  const { results: categoriesResult } = categoriesData;
   const { results: productResult } = productsData;
-
-  const categories = categoriesResult.map((category) => (
-    <Box key={category.id} mx="xl">
-      <Link to={`/store/${category.key}`}>{category.name['en-US']}</Link>
-    </Box>
-  ));
 
   const productCards = productResult.map((product) => {
     const { key } = product;
@@ -38,16 +32,15 @@ const CatalogPage: FC = () => {
     <Box className="wrapper">
       <Box className="middleContainer">
         <Breadcrumbs />
-        <Title c="bright">Store page</Title>
 
-        <Title c="bright" mb={20} mt={16} order={2}>
-          Categories
+        <Tabs />
+
+        <Title c="bright" mt={20}>
+          Store page
         </Title>
-        {categories}
         <Title c="bright" mb={20} mt={16} order={2}>
           Products
         </Title>
-
         <SimpleGrid cols={{ base: 1, sm: 3, xs: 2 }} mt="xl" spacing="60">
           {productCards}
         </SimpleGrid>
