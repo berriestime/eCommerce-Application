55f2a5bb73594cbba3983da39d5b1e80354524b2
Clukva <berries.whale@gmail.com>
2024-05-30 10:22:03 +0300

    feat: add simple style to filter price

--- src/features/catalog/components/filters/filters.tsx
+++ src/features/catalog/components/filters/filters.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { RangeSlider } from '@mantine/core';
+import { RangeSlider, SimpleGrid } from '@mantine/core';
+
+import classes from './filters.module.css';
 
 const Filters = (): JSX.Element => {
   const navigate = useNavigate();
@@ -10,27 +12,35 @@ const Filters = (): JSX.Element => {
   const priceFrom = parseInt(searchParams.get('priceFrom') ?? '') / 100;
   const priceTo = parseInt(searchParams.get('priceTo') ?? '') / 100;
 
-  const [value, setValue] = useState<[number, number]>([priceFrom || 0, priceTo || 100]);
+  const [value, setValue] = useState<[number, number]>([priceFrom || 0, priceTo || 2500]);
 
   return (
-    <div>
-      <RangeSlider
-        labelAlwaysOn
-        onChange={([priceFrom, priceTo]) => {
-          setValue([priceFrom, priceTo]);
-        }}
-        onChangeEnd={([priceFrom, priceTo]) => {
-          const targetSearchParams = new URLSearchParams(location.search);
-          for (const [key, value] of targetSearchParams.entries()) {
-            console.log(key, value);
-          }
+    <div className={classes.contentWrapper}>
+      <SimpleGrid cols={3} flex={1}>
+        <div>
+          Filter by price
+          <RangeSlider
+            label={(value) => `${value} $`}
+            max={2500}
+            min={0}
+            onChange={([priceFrom, priceTo]) => {
+              setValue([priceFrom, priceTo]);
+            }}
+            onChangeEnd={([priceFrom, priceTo]) => {
+              const targetSearchParams = new URLSearchParams(location.search);
+              for (const [key, value] of targetSearchParams.entries()) {
+                console.log(key, value);
+              }
 
-          targetSearchParams.set('priceFrom', (priceFrom * 100).toString());
-          targetSearchParams.set('priceTo', (priceTo * 100).toString());
-          navigate(`?${targetSearchParams.toString()}`);
-        }}
-        value={value}
-      />
+              targetSearchParams.set('priceFrom', (priceFrom * 100).toString());
+              targetSearchParams.set('priceTo', (priceTo * 100).toString());
+              navigate(`?${targetSearchParams.toString()}`);
+            }}
+            step={1}
+            value={value}
+          />
+        </div>
+      </SimpleGrid>
     </div>
   );
 };
