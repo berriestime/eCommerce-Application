d9310b2b28b7bfade6edde479b102684da155e7d
Naya252 <naya252naya@gmail.com>
2024-06-08 16:44:04 +0300

    feat: add type forn cart product

--- src/types/productTypes.ts
+++ src/types/productTypes.ts
@@ -1,4 +1,12 @@
+import { type ProductProjection } from '@commercetools/platform-sdk';
+
 export type Prices = {
   discountPrice: null | string;
   price: string;
+};
+
+export type CartProduct = {
+  product: ProductProjection;
+  quantity: number;
+  url: string;
 };
