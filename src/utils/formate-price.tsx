83dfaf7bc606031b4738961aeb1a1ca823134d85
Clukva <berries.whale@gmail.com>
2024-06-09 01:04:00 +0300

    feat: fetch or create active cart, delete US country product price restriction, add/remove button UI

--- src/utils/formate-price.tsx
+++ src/utils/formate-price.tsx
@@ -2,14 +2,19 @@ import type { ProductProjection } from '@commercetools/platform-sdk';
 
 import type { Prices } from '@/types/productTypes';
 
-const fomatPrice = (price: string, digit: number): string => {
+const formatPrice = (price: string, digit: number): string => {
   const formattedPrice = `${price.slice(0, digit)}.${price.slice(digit)}`;
   return formattedPrice;
 };
 
 export const getPrice = (productData: ProductProjection): Prices => {
-  const { prices } = productData.masterVariant;
-  const curPriceData = prices?.find((price) => price.country === 'US');
+  const curPriceData = productData.masterVariant.prices?.[0];
+  if (!curPriceData) {
+    return {
+      discountPrice: '0.00',
+      price: '0.00',
+    };
+  }
 
   const curPrice = String(curPriceData?.value.centAmount);
   const curDiscountPrice = String(curPriceData?.discounted?.value.centAmount);
@@ -17,11 +22,11 @@ export const getPrice = (productData: ProductProjection): Prices => {
 
   const pricesData: Prices = {
     discountPrice: null,
-    price: fomatPrice(curPrice, digit),
+    price: formatPrice(curPrice, digit),
   };
 
   if (curDiscountPrice !== 'undefined') {
-    pricesData.discountPrice = fomatPrice(curDiscountPrice, digit);
+    pricesData.discountPrice = formatPrice(curDiscountPrice, digit);
   }
 
   return pricesData;
