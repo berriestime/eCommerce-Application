60c68dce0868a2023988537620006ac78cc714f9
NadyaGus <ftotkexit@gmail.com>
2024-06-11 13:18:00 +0300

    feat: add merge anonymous cart into customer cart

--- src/features/login-page/api/api.tsx
+++ src/features/login-page/api/api.tsx
@@ -1,10 +1,20 @@
 import type { ClientResponse, CustomerSignin } from '@commercetools/platform-sdk';
 
+import { getActiveCart } from '@/features/cart/api';
 import { createPasswordFlowClient } from '@/lib/commerstools/create-password-client';
 
-const postCustomerLogin = (customer: CustomerSignin): Promise<ClientResponse> => {
+const postCustomerLogin = async (customer: CustomerSignin): Promise<ClientResponse> => {
+  const cart = await getActiveCart();
   const apiRoot = createPasswordFlowClient(customer);
-  return apiRoot.login().post({ body: customer }).execute();
+
+  const action: CustomerSignin = {
+    anonymousCart: { id: cart.id, typeId: 'cart' },
+    anonymousCartSignInMode: 'MergeWithExistingCustomerCart',
+    email: customer.email,
+    password: customer.password,
+  };
+
+  return apiRoot.login().post({ body: action }).execute();
 };
 
 export { postCustomerLogin };
