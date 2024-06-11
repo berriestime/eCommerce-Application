d1edaabe7636c085a4a405ec68f9351df5b2233c
NadyaGus <ftotkexit@gmail.com>
2024-06-11 22:20:43 +0300

    feat: change test for team page

--- src/features/team/team-page.test.tsx
+++ src/features/team/team-page.test.tsx
@@ -1,10 +1,19 @@
 import { MemoryRouter } from 'react-router-dom';
 
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
 
 import { customRender, screen } from '@/test-utils';
 
 import { TeamPage } from './team-page';
+
+const IntersectionObserverMock = vi.fn(() => ({
+  disconnect: vi.fn(),
+  observe: vi.fn(),
+  takeRecords: vi.fn(),
+  unobserve: vi.fn(),
+}));
+
+vi.stubGlobal('IntersectionObserver', IntersectionObserverMock);
 
 describe('TeamPage', () => {
   test('should render', () => {
