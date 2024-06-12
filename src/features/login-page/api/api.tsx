a57f3c62790ddd195e3254a6ca2044b24a4cfd08
Clukva <berries.whale@gmail.com>
2024-06-12 23:05:58 +0300

    refactor: forse set cart via login

--- src/features/login-page/api/api.tsx
+++ src/features/login-page/api/api.tsx
@@ -1,10 +1,28 @@
 import type { ClientResponse, CustomerSignin } from '@commercetools/platform-sdk';
 
+import { getActiveCart } from '@/features/cart/api';
+import { forceSetCartState } from '@/features/cart/store/cart-slice';
 import { createPasswordFlowClient } from '@/lib/commerstools/create-password-client';
+import { store } from '@/store';
 
-const postCustomerLogin = (customer: CustomerSignin): Promise<ClientResponse> => {
+const postCustomerLogin = async (customer: CustomerSignin): Promise<ClientResponse> => {
+  const cart = await getActiveCart().catch();
   const apiRoot = createPasswordFlowClient(customer);
-  return apiRoot.login().post({ body: customer }).execute();
+
+  if (!cart) {
+    return await apiRoot.login().post({ body: customer }).execute();
+  }
+
+  const action: CustomerSignin = {
+    anonymousCart: { id: cart.id, typeId: 'cart' },
+    anonymousCartSignInMode: 'MergeWithExistingCustomerCart',
+    email: customer.email,
+    password: customer.password,
+  };
+
+  const response = await apiRoot.login().post({ body: action }).execute();
+  store.dispatch(forceSetCartState(response.body.cart));
+  return response;
 };
 
 export { postCustomerLogin };
