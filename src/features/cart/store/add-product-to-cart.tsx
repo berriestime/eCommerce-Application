1a1be9837440488fb0f464f37dd8b0d471096820
Clukva <berries.whale@gmail.com>
2024-06-12 12:43:37 +0300

    refactor: move cart creation to addProductToCart action

--- src/features/cart/store/add-product-to-cart.tsx
+++ src/features/cart/store/add-product-to-cart.tsx
@@ -1,35 +1,42 @@
-import type { CartUpdateAction } from '@commercetools/platform-sdk';
+import type { Cart, CartUpdateAction } from '@commercetools/platform-sdk';
 
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import type { CartItem, CartState } from './types';
 
-import { postCartWithId } from '../api';
+import { createNewCart, postCartWithId } from '../api';
+
+const getCart = async (getState: () => unknown): Promise<Pick<Cart, 'id' | 'version'>> => {
+  const state = getState() as { cart: CartState };
+  const { id, version } = state.cart;
+
+  if (id && version !== undefined) {
+    return { id, version };
+  }
+  {
+    const { id, version } = await createNewCart();
+    return { id, version };
+  }
+};
 
 // Async thunk to add a product to the cart
 const addProductToCart = createAsyncThunk(
   'cart/addProduct',
   async (product: CartItem, { getState, rejectWithValue }) => {
-    const state = getState() as { cart: CartState };
-    const { id, version } = state.cart;
-
-    if (!id || version === undefined) {
-      return rejectWithValue('Cart ID or version is missing');
-    }
-
-    const updateActions: CartUpdateAction[] = [
-      {
-        action: 'addLineItem',
-        productId: product.productId,
-        quantity: product.quantity,
-        variantId: product.variantId,
-      },
-    ];
-
     try {
+      const { id, version } = await getCart(getState);
+      const updateActions: CartUpdateAction[] = [
+        {
+          action: 'addLineItem',
+          productId: product.productId,
+          quantity: product.quantity,
+          variantId: product.variantId,
+        },
+      ];
+
       return postCartWithId(id, updateActions, version);
     } catch (error) {
-      return rejectWithValue(error);
+      return rejectWithValue(String(error));
     }
   },
 );
