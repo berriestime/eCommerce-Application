2d494c08104ce0a8d4ccfece2f1068e70ab7fa55
Clukva <berries.whale@gmail.com>
2024-06-17 23:35:52 +0300

    feat: add addinional notifications for promo codes input

--- src/features/cart/store/apply-promo-code.ts
+++ src/features/cart/store/apply-promo-code.ts
@@ -2,6 +2,8 @@ import type { CartUpdateAction } from '@commercetools/platform-sdk';
 
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+import { PROMO_3 } from '@/constants/catalog-constants';
+
 import type { CartState } from './types';
 
 import { postCartWithId } from '../api';
@@ -10,10 +12,20 @@ const applyPromoCode = createAsyncThunk(
   'cart/apply-promo-code',
   async (code: string, { getState, rejectWithValue }) => {
     const state = getState() as { cart: CartState };
-    const { id, version } = state.cart;
+
+    const { discountCodes, id, totalPriceAfterCatalogDiscountRaw, version } = state.cart;
 
     if (!id || version === undefined) {
       return rejectWithValue('Cart ID or version is missing');
+    }
+
+    if (discountCodes.some((x) => x.code === code)) {
+      return rejectWithValue('This promo code has already been applied');
+    }
+
+    const minPriceToApply = 300000;
+    if (code === PROMO_3.promocode && totalPriceAfterCatalogDiscountRaw < minPriceToApply) {
+      return rejectWithValue(`There are not enough products in the cart for the discount`);
     }
 
     const updateActions: CartUpdateAction[] = [
